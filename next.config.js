95d75fcc412f7d0db79dd71c9767e83531a9665d
Olalekan Animashaun <kim.olalekan@gmail.com>
2021-10-01 03:34:23 +0100

    Downgrade to webpack 4

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
-  webpack5: true,
+  webpack5: false,
   webpack: (config) => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
