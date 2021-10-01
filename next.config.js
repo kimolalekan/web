75a21351e86da49cd7ebf47ffe853c41b6fdda87
Olalekan Animashaun <kim.olalekan@gmail.com>
2021-10-01 03:44:55 +0100

    Upgrade to webpack 5

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
-  webpack5: false,
+  webpack5: true,
   webpack: (config) => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
